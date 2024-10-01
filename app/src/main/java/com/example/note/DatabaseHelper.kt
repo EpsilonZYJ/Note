63322c060c83663e0953b688cbb9deec303a4dff
HelloWorldWin <143871992+EpsilonZYJ@users.noreply.github.com>
2024-10-01 11:01:38 +0800

    Database design

--- app/src/main/java/com/example/note/DatabaseHelper.kt
+++ app/src/main/java/com/example/note/DatabaseHelper.kt
@@ -10,5 +10,28 @@ class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME
         private const val DATABASE_NAME = "AccountBook.db"
         private const val DATABASE_VERSION = 1
         private const val TABLE_NAME = "account_records"
+        private const val COLUMN_ID = "id"
+        private const val COLUMN_DATE = "date"
+        private const val COLUMN_AMOUNT = "amount"
+        private const val COLUMN_PURPOSE = "purpose"
+        private const val COLUMN_TAG = "tag"
+    }
+
+    override fun onCreate(p0: SQLiteDatabase) {
+        val createTableQuery = """
+            CREATE TABLE $TABLE_NAME(
+                $COLUMN_ID TEXT PRIMARY KEY,
+                $COLUMN_DATE TEXT,
+                $COLUMN_AMOUNT REAL,
+                $COLUMN_PURPOSE TEXT,
+                $COLUMN_TAG TEXT
+            )
+        """.trimIndent()
+        p0.execSQL(createTableQuery)
+    }
+
+    override fun onOpen(db: SQLiteDatabase) {
+        db.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+        onCreate(db)
     }
 }
